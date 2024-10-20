1f3688f55ace4febff63eac9b142210060dd808e
Swetabh333 <swetabh333@gmail.com>
2024-10-21 03:08:32 +0530

    adding routes/auth

--- app/main/main.go
+++ app/main/main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 
 	"github.com/Swetabh333/Makerble/app/databases"
+	"github.com/Swetabh333/Makerble/app/models"
 	"github.com/Swetabh333/Makerble/app/routes"
+	//"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -13,14 +15,25 @@ func main() {
 	godotenv.Load(".env")
 
 	//Connect to the database and return db instance
-	_, err := databases.ConnectToDatabase()
+	db, err := databases.ConnectToDatabase()
 	if err != nil {
 		log.Fatal("Could not connect to the database")
 	}
 	log.Print("connected to the database")
 
+	//Migrating data to postgres
+	err = db.AutoMigrate(&models.User{}, &models.Doctor{}, &models.Patient{})
+	if err != nil {
+		log.Fatal("Error migrating data to database")
+	}
+
+	//gin.SetMode(gin.ReleaseMode)
+
 	//Create a router for routing requests
 	router := routes.NewRouter()
+
+	//routes
+	router.GET("/ping", routes.HandlePong)
 
 	//Start out http server at port 8080
 	router.Run("0.0.0.0:8080")
--- app/models/models.go
+++ app/models/models.go
@@ -9,11 +9,20 @@ type User struct {
 	Role     string    `gorm:"size:255;not null"`
 }
 
+type Doctor struct {
+	ID         uuid.UUID `gorm:"primary key"`
+	UserID     uuid.UUID `gorm:"not null"`
+	Name       string    `gorm:"not null"`
+	Speciality string
+	Patients   []Patient
+	User       User `gorm:"foreignKey:UserID"`
+}
+
 type Patient struct {
 	ID       uuid.UUID `gorm:"primary key"`
 	Name     string    `gorm:"primary key;size:255;not null;unique"`
 	Age      int       `gorm:"not null"`
 	Gender   string
 	DoctorID uuid.UUID `gorm:"not null"`
-	User     User      `gorm:"foreignKey:DoctoID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
+	Doctor   Doctor    `gorm:"foreignKey:DoctorID;constraint:OnUpdate:CASCADE;OnDelete:CASCADE"`
 }
